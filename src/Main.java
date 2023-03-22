3b1a6d9687518e0b09a2c6011501d35be41e75cf
Jiraplus123 <plubjiraplus@gmail.com>
2023-03-22 12:04:55 +0700

    last update

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello WSM");
         Scanner sc1 = new Scanner(System.in);
         String name = sc1.nextLine();
         System.out.println(name);
 
         System.out.println("Hello Olarn");
+=======
+        System.out.println("Hello world!");
+>>>>>>> parent of dd7049e (updated file)
     }
 }
