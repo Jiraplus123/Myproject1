2e2916fcfb22db9ae61015901a2b6b810aa622f5
Jiraplus123 <plubjiraplus@gmail.com>
2023-03-22 11:17:35 +0700

    modified file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello WSM");
+        Scanner sc1 = new Scanner(System.in);
+        String name = sc1.nextLine();
+        System.out.println(name);
     }
 }
