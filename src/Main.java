c4cd86006246d86a5994be890ff6f6f02a7ced41
Jiraplus123 <plubjiraplus@gmail.com>
2023-03-22 11:32:18 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
 
         System.out.println("Hello world! and my friends");
+=======
+        System.out.println("Hello world!");
+>>>>>>> 9210fe1 (updated file)
     }
 }
