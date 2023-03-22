4a522b839a8f791644bb66f85965e9c08ebd657e
Jiraplus123 <plubjiraplus@gmail.com>
2023-03-22 11:45:25 +0700

    updated file

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-
         System.out.println("Hello world! and my friends");
-=======
         System.out.println("Hello world!");
->>>>>>> 9210fe1 (updated file)
+
     }
 }
